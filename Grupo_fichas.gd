bd1b6140171f5601164b6a4202aca66b1d2c13ba
Dián <148564332+Dian-OM@users.noreply.github.com>
2026-03-17 12:49:43 +0100

    commit para hacer pull

--- Grupo_fichas.gd
+++ Grupo_fichas.gd
@@ -6,14 +6,22 @@ signal no_sobre_mi
 
 # Agrupación de cartas, usado para verificar correcta posición de la mesa
 # y para hacer que la posición de las fichas en el mismo grupo sea contigua
+# Se le pueden añadir y quitar fichas, puede partirse en dos (devolviendo el nuevo grupo de fichas creado)
+# Posee dos area2D que permiten a manager_fichas darle una carta o cartas nuevas
 
-var escena = preload("res://proyecto_rummikub/ficha/grupo_fichas.tscn")
+const escena = preload("res://proyecto_rummikub/ficha/grupo_fichas.tscn")
 const tamano_extra = 1.5
 
 var fichas : Array[Ficha] = []
 var anchura_hitbox = Ficha.tamano_ficha_static().x * tamano_extra
 
-func recalcula_anchura() -> void:
+static func Grupo_fichas(listaFichas : Array[Ficha]) -> Grupo_fichas:
+	var grupo  : Grupo_fichas = escena.instantiate()
+	for ficha in listaFichas :
+		grupo.anadir_ficha_fin(ficha)
+	return grupo
+
+func _recalcula_anchura() -> void:
 	$izquierda/CollisionShape2D.shape.size.x = anchura_hitbox / 2
 	$derecha/CollisionShape2D.shape.size.x = anchura_hitbox / 2
 	$izquierda/CollisionShape2D.position.x = -anchura_hitbox / 4
@@ -24,16 +32,29 @@ func recalcula_anchura() -> void:
 func _ready() -> void:
 	$izquierda/CollisionShape2D.shape.size.y = Ficha.tamano_ficha_static().y
 	$derecha/CollisionShape2D.shape.size.y = Ficha.tamano_ficha_static().y
-	recalcula_anchura()
+	_recalcula_anchura()
 
-func anadir_ficha(ficha : Ficha) -> void:
+func anadir_lista_fin() -> void:
+	pass
+
+func anadir_ficha_fin(ficha : Ficha) -> void:
 	self.add_child(ficha)
-	fichas.append(ficha)
+	fichas.push_back(ficha)
 	ficha.set_grupo(self)
 	anchura_hitbox += ficha.tamano_ficha().x
-	recalcula_anchura()
+	_recalcula_anchura()
 	ficha.transform = Transform2D()
-	posicionar_fichas()
+	_posicionar_fichas()
+
+func anadir_ficha_principio(ficha : Ficha)-> void:
+	self.add_child(ficha)
+	fichas.push_front(ficha)
+	ficha.set_grupo(self)
+	anchura_hitbox += ficha.tamano_ficha().x
+	_recalcula_anchura()
+	ficha.transform = Transform2D()
+	_posicionar_fichas()
+
 
 
 # ficha es la ficha en la cual se está partiendo
@@ -44,13 +65,12 @@ func partir(ficha: Ficha) -> Grupo_fichas:
 	var grupo: Grupo_fichas = escena.instantiate()
 	for i in fichas.size():
 		if fichas[i] == ficha: return grupo
-		
 		grupo.anadir_carta(fichas[i])
 		fichas[i].set_grupo(grupo)
 		fichas.remove_at(i)
 	return grupo
 	
-func posicionar_fichas() -> void:
+func _posicionar_fichas() -> void:
 	var indice = 0
 	for ficha in fichas:
 		print(indice)
